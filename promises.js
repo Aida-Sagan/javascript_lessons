5b53afb7e4a4852a20805c9fe0ec21499b0fde6d
Ayaulym <aya.09nice@gmail.com>
2025-03-09 16:06:42 +0500

    pizza

--- promises.js
+++ promises.js
@@ -1,5 +1,5 @@
 // синхронный код
-console.log('Приветствие!')
+/* console.log('Приветствие!')
 let a = 50 + 100;
 console.log(a)
 console.log('Bye!')
@@ -11,4 +11,31 @@ setTimeout( () => {
     console.log('смс в setTimeout через 3 секунды')
 }, 3000);
 
-console.log('POKA!')
+console.log('POKA!') */
+
+console.log('Заказываем пиццу ...');
+
+const pizzaOrder = new Promise( (resolve, reject) => {
+    setTimeout( () => {
+        let success = Math.random() > 0.5;
+
+        if(success) {
+            resolve('Ура! Пицца доставлена!!!');
+        } else {
+            reject(':( ошибка: курьер потерялся....');
+        }
+    }, 4000)
+}) 
+
+pizzaOrder.then( (message) => {
+    console.log(message)
+})
+
+/**
+ * then(message => console.log(message))
+ */
+
+pizzaOrder.catch(error => console.error(error));
+pizzaOrder.finally(() => console.log('ПРиятного аппетита!'));
+
+console.log('Продолжаем делать другие дела ....')
